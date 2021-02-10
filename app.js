962d227562dab48a9e6b0cacd96767ceaba0bb94
Nuzhat Parveen <nuzhat.gisanalyst@gmail.com>
2021-02-10 02:28:44 -0500

    Project- API

---
+++ app.js
@@ -0,0 +1,16 @@
+
+fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+    .then(res => res.json())
+    .then(data => displayMeals(data.meals))
+
+const displayMeals = meals => {
+    for (let i = 0; i < meals.length; i++) {
+        const meal = meals[i];
+        console.log(meal.strMeal);
+
+    }
+}
+
+
+
+
