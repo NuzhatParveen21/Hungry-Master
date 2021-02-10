3fef8d21eb8a6630523986273522bfa91a1df16c
Nuzhat Parveen <nuzhat.gisanalyst@gmail.com>
2021-02-10 04:54:54 -0500

    Menu List with API

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-    .then(res => res.json())
-    .then(data => displayMeals(data.meals))
+const searchMenu = () => {
+    const searchMeal = document.getElementById('search-menu').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeal}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals))
 
-const displayMeals = meals => {
-    for (let i = 0; i < meals.length; i++) {
-        const meal = meals[i];
-        console.log(meal.strMeal);
 
-    }
 }
+const displayMeals = meals => {
+
+    const menuContainer = document.getElementById("menu-container");
+    meals.forEach(meal => {
+        const menuDiv = document.createElement('div');
+        menuDiv.className = 'menu-list';
+        const menuList = `
+        <img class="img-fluid img-thumbnail mx-auto" src = "$(meal.strMealThumb)" >
+        <h3 class="menu-item">${meal.strMeal}</h3>
+    `;
+        menuDiv.innerHTML = menuList;
+        menuContainer.appendChild(menuDiv)
+    });
+
+}
+
+
+
+
+
+
 
 
 
